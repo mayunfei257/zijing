f848bbe9aeeff87668cfe877c2a542008a316aaf
mayunfei <mayunfei257@sina.com>
2018-04-10 16:00:53 +0800

    外挂法杖TEST……

--- src/test/java/com/zijing/test/Building.java
+++ src/test/java/com/zijing/test/Building.java
@@ -1,10 +1,12 @@
 package com.zijing.test;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockAnvil;
 import net.minecraft.block.BlockCauldron;
 import net.minecraft.block.BlockChest;
 import net.minecraft.block.BlockDoor;
 import net.minecraft.block.BlockFurnace;
+import net.minecraft.block.BlockPlanks;
 import net.minecraft.block.BlockSlab;
 import net.minecraft.block.BlockWoodSlab;
 import net.minecraft.block.state.IBlockState;
@@ -14,6 +16,25 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class Building {
+	
+	private static BlocksUtil blocksUtil;
+	private static Building instance;
+	
+	private Building() {}
+	
+	public static Building getinstance() {
+		if(null == instance) {
+			synchronized (Building.class) {
+				if(null == instance) {
+					instance = new Building();
+					blocksUtil = BlocksUtil.getinstance();
+				}
+			}
+		}
+		return instance;
+	}
+	
+	//*********************************************************************************************************************************************************************
 	
 	public boolean buildASmallHouse(World world, BlockPos baseBlockPos) {
 		IBlockState floor = blocksUtil.end_bricks;
@@ -92,23 +113,88 @@ public class Building {
 				}
 			}
 		}
-		return false;
+		return true;
 	}
-	//*********************************************************************************************************************************************************************
-	private static BlocksUtil blocksUtil;
-	private static Building instance;
 	
-	private Building() {}
-	
-	public static Building getinstance() {
-		if(null == instance) {
-			synchronized (Building.class) {
-				if(null == instance) {
-					instance = new Building();
-					blocksUtil = BlocksUtil.getinstance();
+	/**
+	 * Create arable land at the foot of the block for the center of the arable land; 4 * 4;
+	 * @param world
+	 * @param baseBlockPos
+	 * @return
+	 */
+	public boolean buildArableLand(World world, BlockPos baseBlockPos, Block plant) {
+		IBlockState fieldRoad = Blocks.PLANKS.getDefaultState().withProperty(BlockPlanks.VARIANT, BlockPlanks.EnumType.DARK_OAK);
+		IBlockState waterCover = Blocks.WOODEN_SLAB.getDefaultState().withProperty(BlockWoodSlab.VARIANT, BlockPlanks.EnumType.DARK_OAK).withProperty(BlockWoodSlab.HALF, BlockSlab.EnumBlockHalf.BOTTOM);
+		
+		//arable land
+		for(int x = -20; x <= 20; x++) {
+			for(int z = -20; z <= 20; z++) {
+				if(x % 10 == 0 || z % 10 == 0) {
+					BlockPos blockPos = new BlockPos(baseBlockPos.getX() + x, baseBlockPos.getY(), baseBlockPos.getZ() + z);
+					world.setBlockState(blockPos , fieldRoad, 11);
+					if(world.getBlockState(blockPos.up()).getBlock() != Blocks.AIR) {
+						world.setBlockState(blockPos.up(), Blocks.AIR.getDefaultState(), 11);
+					}
+				}else if(x % 5 == 0 && z % 5 == 0){
+					BlockPos blockPos = new BlockPos(baseBlockPos.getX() + x, baseBlockPos.getY(), baseBlockPos.getZ() + z);
+					world.setBlockState(blockPos, Blocks.WATER.getDefaultState(), 11);
+					world.setBlockState(blockPos.up(), waterCover, 11);
+				}else {
+					BlockPos blockPos = new BlockPos(baseBlockPos.getX() + x, baseBlockPos.getY(), baseBlockPos.getZ() + z);
+					world.setBlockState(blockPos, Blocks.FARMLAND.getDefaultState(), 11);
+					if(null != plant) {
+						world.setBlockState(blockPos.up(), plant.getDefaultState(), 11);
+					}else if(world.getBlockState(blockPos.up()).getBlock() != Blocks.AIR) {
+						world.setBlockState(blockPos.up(), Blocks.AIR.getDefaultState(), 11);
+					}
 				}
 			}
 		}
-		return instance;
+		return true;
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
